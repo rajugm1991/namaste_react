bf2c2038b2371fac26dd83afa4a689c5bbd78782
rajugm <rajugm1991>
2023-03-24 17:36:04 +0530

    episode-2

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
+
 /**
  * 
  * <div id ="parent">
@@ -13,6 +16,28 @@
  * 
  */
 
+
+/**
+ *  Learnings from episode 2
+ * 
+ *  Git setup local
+ *  Remote git code push
+ *  npm init -> pulled package manager
+ *  parcel -> File bundlers similar to Webpack and others
+ *  npm install parcel -> package.json creation where it tracks dependency verstion
+ *  package.lock.json -> Keep track of exact version
+ *  tildo -> used to auto upgrade
+ *  caret -> used to minot upgrade
+ *  npm install react and dom
+ *  switched react to npm module in code
+ *  cdn react -> It requires to load every time from script.
+ *  import will refer to node module folder.
+ *  
+ *  
+ * 
+ * 
+ * 
+ */
 
 const parent=React.createElement("div",{id:'parent'},
 React.createElement('div',{id: 'child'},
